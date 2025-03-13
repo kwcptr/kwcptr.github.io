91a73c155fe4cf6d754218a973c131ccbc833430
BonaInnadril <114345218+BonaInnadril@users.noreply.github.com>
2025-03-13 11:03:09 -0300

    Upd

--- src/components/pages/hero/Hero.tsx
+++ src/components/pages/hero/Hero.tsx
@@ -1,28 +1,38 @@
-/* Img */
-// import logo from './img/logo.png'
-/* Components */
-// import {Search} from 'components'
 import { useEffect } from "react";
 import ReactGA from "react-ga4";
-import videoBg from "./img/videoBg.mp4"
 import Changelog from "components/globals/changelog/Changelog";
-export const Hero = () => {
 
+export const Hero = () => {
     useEffect(() => {
         ReactGA.event({
             category: "pagedirectories",
             action: "load",
             label: "home",
         });
-        }, []);
+    }, []);
 
     return (
         <section className="section__hero">
             <div className="hero__container">
                 <h2 className="container__name">Welcome!</h2>
-                <h3 className="container__name2">Use the Menu or the Search bar to look for info.</h3>
+                <h3 className="container__name2">
+                    Use the Menu or the Search bar to look for info.
+                </h3>
                 <Changelog /> {/* Render the Changelog component here */}
             </div>
+
+            {/* Add a section for the Discord widget */}
+            <div className="discord__widget">
+                <iframe
+                    title="Discord"
+                    src="https://discord.com/widget?id=673917009897062411&theme=dark"
+                    width="350"
+                    height="400"
+                    allowTransparency={true}
+                    frameBorder="0"
+                    sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+                ></iframe>
+            </div>
         </section>
-    )
-}
+    );
+};
