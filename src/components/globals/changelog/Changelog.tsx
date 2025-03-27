989c5e0a89e6246a53117f85e9b971f75c9faea0
BonaInnadril <114345218+BonaInnadril@users.noreply.github.com>
2025-03-27 11:23:41 -0300

    Upd

--- src/components/globals/changelog/Changelog.tsx
+++ src/components/globals/changelog/Changelog.tsx
@@ -8,6 +8,10 @@ const Changelog = () => {
       </div>
       <div className="changelog__content">
         <div className="changelog__bubble">
+          <p className="changelog__subtitle">27/03/25</p>
+          <p className="changelog__text">
+            — Updated item: <a href="/items/armor/cloak/heavenly-cloak" className="changelog__link">Heavenly Cloak</a>
+          </p>
           <p className="changelog__subtitle">21/03/25</p>
           <p className="changelog__text">
             — New items: <a href="/items/soul-crystal/adens-transcendent-soul-crystal-weapon" className="changelog__link">Aden's Transcendent Soul Crystal - Weapon</a>, <a href="/items/soul-crystal/adens-transcendent-soul-crystal-armor" className="changelog__link">Aden's Transcendent Soul Crystal - Armor</a>, <a href="/items/soul-crystal/hardins-transcendent-soul-crystal" className="changelog__link">Hardin's Transcendent Soul Crystal</a>
--- src/templates/cloaks/heavenly-cloak.tsx
+++ src/templates/cloaks/heavenly-cloak.tsx
@@ -1,32 +1,316 @@
-import { Table, Title } from "components";
-import { HEAVENLY_CLOAK_ENCHANT_V1, HEAVENLY_CLOAK_TRIGGER_V1, HEAVENLY_CLOAK_V1 } from "data";
+import { Title } from "components";
 
 export const HeavenlyCloak = () => {
 
   return (
     <div>
+      <div className="section__items2">
         <Title
           label="Heavenly Cloak"
           image="/items/icons/cloak/heavenly-cloak.png"
         />
-        <div className="contain">
-          <div className="table__container">
-
-            <div className="container__subTable">
-              <h3 className="subTitle">Enchant</h3>
-              <Table data={HEAVENLY_CLOAK_ENCHANT_V1} />
-            </div>
-            <div className="container__subTable">
-              <h3 className="subTitle">Enchant Effects</h3>
-              <Table data={HEAVENLY_CLOAK_V1} />
-            </div>
-            <div className="container__subTable">
-              <h3 className="subTitle">Trigger skill: Angelic Recovery</h3>
-              <Table data={HEAVENLY_CLOAK_TRIGGER_V1} />
-            </div>
-          </div>
-        </div>
-
-        </div>
+        <table className="article-table">
+          <thead>
+            <tr>
+              <th colSpan={2}>Enchant</th>
+              <th rowSpan={2}>Base effect</th>
+              <th rowSpan={2}>Special effect</th>
+              <th rowSpan={2}>Cloak appearance</th></tr>
+            <tr>
+              <th>Lv.</th>
+              <th>Success rate</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>-</td>
+              <td>-</td>
+              <td>
+                Max HP +20%<br />
+                Max MP +20%<br />
+                Resistance to All Weapons +15%<br />
+                Acquired XP +30%<br />
+                Acquired SP +30%<br />
+                P. Atk. +500<br />
+                M. Atk. +500<br />
+                P. Def. +15%<br />
+                M. Def. +15%<br />
+                All Critical Damage +15%<br />
+                All received Critical Rate -10%<br />
+                All received Critical Damage -10%<br />
+                Received fixed damage -5%<br />
+                Unequip Resistance +20%<br /><br />
+              </td>
+              <td>
+                <span style={{ color: '#FF8000' }}>Angelic Recovery</span><br />
+                Buff effect: When hit, Recovers 333 HP/ 167 MP every 3.3 sec., Speed +33<br />
+                Trigger rate: 1%<br />
+                Duration: 10 sec.<br />
+                Trigger cooldown: 120 sec.
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>+1</td>
+              <td>50%</td>
+              <td>
+                Max HP +21%<br />
+                Max MP +21%<br />
+                Resistance to All Weapons +16%<br />
+                Acquired XP +30%<br />
+                Acquired SP +30%<br />
+                P. Atk. +1000<br />
+                M. Atk. +1000<br />
+                P. Def. +15%<br />
+                M. Def. +15%<br />
+                All Critical Damage +15%<br />
+                All received Critical Rate -10%<br />
+                All received Critical Damage -10%<br />
+                Received fixed damage -6%<br />
+                Unequip Resistance +22%<br /><br />
+              </td>
+              <td>
+                <span style={{ color: '#FF8000' }}>Angelic Recovery</span><br />
+                Buff effect: When hit, Recovers 333 HP/ 167 MP every 3.3 sec., Speed +33<br />
+                Trigger rate: 3%<br />
+                Duration: 10 sec.<br />
+                Trigger cooldown: 120 sec.
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>+2</td>
+              <td>40%</td>
+              <td>
+                Max HP +22%<br />
+                Max MP +22%<br />
+                Resistance to All Weapons +17%<br />
+                Acquired XP +30%<br />
+                Acquired SP +30%<br />
+                P. Atk. +1500<br />
+                M. Atk. +1500<br />
+                P. Def. +15%<br />
+                M. Def. +15%<br />
+                All Critical Damage +15%<br />
+                All received Critical Rate -10%<br />
+                All received Critical Damage -10%<br />
+                Received fixed damage -7%<br />
+                Unequip Resistance +24%<br /><br />
+              </td>
+              <td>
+                <span style={{ color: '#FF8000' }}>Angelic Recovery</span><br />
+                Buff effect: When hit, Recovers 333 HP/ 167 MP every 3.3 sec., Speed +33<br />
+                Trigger rate: 5%<br />
+                Duration: 10 sec.<br />
+                Trigger cooldown: 120 sec.
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>+3</td>
+              <td>30%</td>
+              <td>
+                Max HP +23%<br />
+                Max MP +23%<br />
+                Resistance to All Weapons +18%<br />
+                Acquired XP +30%<br />
+                Acquired SP +30%<br />
+                P. Atk. +2000<br />
+                M. Atk. +2000<br />
+                P. Def. +15%<br />
+                M. Def. +15%<br />
+                All Critical Damage +15%<br />
+                All received Critical Rate -10%<br />
+                All received Critical Damage -10%<br />
+                Received fixed damage -8%<br />
+                Skill Power +1%<br />
+                Unequip Resistance +26%<br /><br />
+              </td>
+              <td>
+                <span style={{ color: '#FF8000' }}>Angelic Recovery</span><br />
+                Buff effect: When hit, Recovers 333 HP/ 167 MP every 3.3 sec., Speed +33<br />
+                Trigger rate: 7%<br />
+                Duration: 10 sec.<br />
+                Trigger cooldown: 120 sec.
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>+4</td>
+              <td>20%</td>
+              <td>
+                Max HP +24%<br />
+                Max MP +24%<br />
+                Resistance to All Weapons +19%<br />
+                Acquired XP +50%<br />
+                Acquired SP +50%<br />
+                P. Atk. +2500<br />
+                M. Atk. +2500<br />
+                P. Def. +15%<br />
+                M. Def. +15%<br />
+                All Critical Damage +15%<br />
+                All received Critical Rate -10%<br />
+                All received Critical Damage -10%<br />
+                Received fixed damage -9%<br />
+                Skill Power +2%<br />
+                Unequip Resistance +28%<br /><br />
+              </td>
+              <td>
+                <span style={{ color: '#FF8000' }}>Angelic Recovery</span><br />
+                Buff effect: When hit, Recovers 333 HP/ 167 MP every 3.3 sec., Speed +33<br />
+                Trigger rate: 9%<br />
+                Duration: 10 sec.<br />
+                Trigger cooldown: 120 sec.
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>+5</td>
+              <td>10%</td>
+              <td>
+                Max HP +25%<br />
+                Max MP +25%<br />
+                Resistance to All Weapons +20%<br />
+                Acquired XP +55%<br />
+                Acquired SP +55%<br />
+                P. Atk. +4000<br />
+                M. Atk. +4000<br />
+                P. Def. +15%<br />
+                M. Def. +15%<br />
+                All Critical Damage +15%<br />
+                All received Critical Rate -10%<br />
+                All received Critical Damage -10%<br />
+                Received fixed damage -10%<br />
+                Skill Power +3%<br />
+                Unequip Resistance +30%<br /><br />
+              </td>
+              <td>
+                <span style={{ color: '#FF8000' }}>Angelic Recovery</span><br />
+                Buff effect: When hit, Recovers 333 HP/ 167 MP every 3.3 sec., Speed +33<br />
+                Trigger rate: 12%<br />
+                Duration: 10 sec.<br />
+                Trigger cooldown: 120 sec.<br /><br />
+                <span style={{ color: '#FF8000' }}>Angelic Guard</span><br />
+                Buff effect: When hit, Damage from skills with 2+ attacks -5%<br />
+                Trigger rate: 1%<br />
+                Duration: 5 sec.<br />
+                Trigger cooldown: 600 sec.
+              </td>
+              <td>
+                <video src="https://vod.plaync.com/lineage2/update/250321/item_01.webm" playsInline autoPlay loop muted className="kit-webm__screen"></video>
+              </td>
+            </tr>
+            <tr>
+              <td>+6</td>
+              <td>10%</td>
+              <td>
+                Max HP +26%<br />
+                Max MP +26%<br />
+                Resistance to All Weapons +20%<br />
+                Acquired XP +60%<br />
+                Acquired SP +60%<br />
+                P. Atk. +6000<br />
+                M. Atk. +6000<br />
+                P. Def. +15%<br />
+                M. Def. +15%<br />
+                All Critical Damage +15%<br />
+                All received Critical Rate -10%<br />
+                All received Critical Damage -10%<br />
+                Received fixed damage -11%<br />
+                Skill Power +4%<br />
+                Unequip Resistance +32%<br /><br />
+              </td>
+              <td>
+                <span style={{ color: '#FF8000' }}>Angelic Recovery</span><br />
+                Buff effect: When hit, Recovers 333 HP/ 167 MP every 3.3 sec., Speed +33,<br />Hold/ Paralysis/ Suppression Resistance +20%<br />
+                Trigger rate: 13%<br />
+                Duration: 10 sec.<br />
+                Trigger cooldown: 120 sec.<br /><br />
+                <span style={{ color: '#FF8000' }}>Angelic Guard</span><br />
+                Buff effect: When hit, Damage from skills with 2+ attacks -10%<br />
+                Trigger rate: 2%<br />
+                Duration: 5 sec.<br />
+                Trigger cooldown: 600 sec.
+              </td>
+              <td>
+                <video src="https://vod.plaync.com/lineage2/update/250321/item_02.webm" playsInline autoPlay loop muted className="kit-webm__screen"></video>
+              </td>
+            </tr>
+            <tr>
+              <td>+7</td>
+              <td>10%</td>
+              <td>
+                Max HP +27%<br />
+                Max MP +27%<br />
+                Resistance to All Weapons +20%<br />
+                Acquired XP +65%<br />
+                Acquired SP +65%<br />
+                P. Atk. +8000<br />
+                M. Atk. +8000<br />
+                P. Def. +15%<br />
+                M. Def. +15%<br />
+                All Critical Damage +15%<br />
+                All received Critical Rate -10%<br />
+                All received Critical Damage -10%<br />
+                Received fixed damage -12%<br />
+                Skill Power +5%<br />
+                Unequip Resistance +34%<br /><br />
+              </td>
+              <td>
+                <span style={{ color: '#FF8000' }}>Angelic Recovery</span><br />
+                Buff effect: When hit, Recovers 333 HP/ 167 MP every 3.3 sec., Speed +33,<br />Hold/ Paralysis/ Suppression Resistance +20%<br />
+                Trigger rate: 14%<br />
+                Duration: 10 sec.<br />
+                Trigger cooldown: 120 sec.<br /><br />
+                <span style={{ color: '#FF8000' }}>Angelic Guard</span><br />
+                Buff effect: When hit, Damage from skills with 2+ attacks -15%<br />
+                Trigger rate: 3%<br />
+                Duration: 5 sec.<br />
+                Trigger cooldown: 600 sec.
+              </td>
+              <td>
+                <video src="https://vod.plaync.com/lineage2/update/250321/item_03.webm" playsInline autoPlay loop muted className="kit-webm__screen"></video>
+              </td>
+            </tr>
+            <tr>
+              <td>+8</td>
+              <td>10%</td>
+              <td>
+                Max HP +28%<br />
+                Max MP +28%<br />
+                Resistance to All Weapons +20%<br />
+                Acquired XP +70%<br />
+                Acquired SP +70%<br />
+                P. Atk. +10000<br />
+                M. Atk. +10000<br />
+                P. Def. +15%<br />
+                M. Def. +15%<br />
+                All Critical Damage +15%<br />
+                All received Critical Rate -10%<br />
+                All received Critical Damage -10%<br />
+                Received fixed damage -13%<br />
+                Skill Power +7%<br />
+                Unequip Resistance +35%<br /><br />
+              </td>
+              <td>
+                <span style={{ color: '#FF8000' }}>Angelic Recovery</span><br />
+                Buff effect: When hit, Recovers 333 HP/ 167 MP every 3.3 sec., Speed +33,<br />Hold/ Paralysis/ Suppression Resistance +20%<br />
+                Trigger rate: 15%<br />
+                Duration: 10 sec.<br />
+                Trigger cooldown: 120 sec.<br /><br />
+                <span style={{ color: '#FF8000' }}>Angelic Guard</span><br />
+                Buff effect: When hit, Damage from skills with 2+ attacks -20%<br />
+                Trigger rate: 5%<br />
+                Duration: 5 sec.<br />
+                Trigger cooldown: 600 sec.
+              </td>
+              <td>
+                <video src="https://vod.plaync.com/lineage2/update/250321/item_04.webm" playsInline autoPlay loop muted className="kit-webm__screen"></video>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
